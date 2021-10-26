824f3178167234cad40d6f356a9f31585cc82a8c
Yasser Hajlaoui <yasser159@gmail.com>
2021-10-26 14:02:02 -0700

    displays 3 items on screen

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -9,12 +9,30 @@
 import UIKit
 
 class TodoListViewController: UITableViewController {
-
+    //Array contains items that will show up on the screen
+    let itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogotgon" ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    //MARK - Tableview Datasource Methods
+    
+    //Function returns number of rows in array
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    //Function ???
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
 
 }
 
